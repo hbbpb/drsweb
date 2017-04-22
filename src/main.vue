5731e8032b8a8e3445c36ba9a2d0b4e2b367f556
neoli <hbbpbb@gmail.com>
2017-04-22 20:39:55 +0800

    list view.

--- src/main.vue
+++ src/main.vue
@@ -12,66 +12,21 @@
                 <f7-pages>
                     <f7-page>
                         <f7-list>
-                            <f7-list-item link="/search/" title="文献检索" link-view="#main-view" link-close-panel></f7-list-item>
-                            <f7-list-item link="/my/" title="我的收藏" link-view="#main-view" link-close-panel></f7-list-item>
-                            <f7-list-item link="/setting/" title="系统设置" link-view="#main-view" link-close-panel></f7-list-item>
-                            <f7-list-item link="/help/" title="帮助" link-view="#main-view" link-close-panel></f7-list-item>
-                            <f7-list-item link="/about/" title="关于" link-view="#main-view" link-close-panel></f7-list-item>
+                            <f7-list-item link="/search/" title="文献检索" link-view="#main-view"
+                                          link-close-panel></f7-list-item>
+                            <f7-list-item link="/my/" title="我的收藏" link-view="#main-view"
+                                          link-close-panel></f7-list-item>
+                            <f7-list-item link="/setting/" title="系统设置" link-view="#main-view"
+                                          link-close-panel></f7-list-item>
+                            <f7-list-item link="/help/" title="帮助" link-view="#main-view"
+                                          link-close-panel></f7-list-item>
+                            <f7-list-item link="/about/" title="关于" link-view="#main-view"
+                                          link-close-panel></f7-list-item>
                         </f7-list>
                     </f7-page>
                 </f7-pages>
             </f7-view>
         </f7-panel>
-
-        <!-- Main Views -->
-        <f7-views>
-            <f7-view id="main-view" navbar-through :dynamic-navbar="true" main>
-                <!-- Navbar -->
-                <f7-navbar>
-                    <f7-nav-center sliding>图书馆参考咨询服务</f7-nav-center>
-                    <f7-nav-right>
-                        <f7-link icon="icon-bars" open-panel="right"></f7-link>
-                    </f7-nav-right>
-                </f7-navbar>
-                <!-- Pages -->
-                <f7-pages>
-                    <f7-page>
-                        <f7-block-title>图书馆参考咨询服务</f7-block-title>
-                        <f7-block inner>
-                            <p>Duis sed erat ac eros ultrices pharetra id ut tellus. Praesent rhoncus enim ornare ipsum aliquet ultricies. Pellentesque sodales erat quis elementum sagittis.</p>
-                        </f7-block>
-                        <f7-block-title>Navigation</f7-block-title>
-                        <f7-list>
-                            <f7-list-item link="/about/" title="About"></f7-list-item>
-                            <f7-list-item link="/form/" title="Form"></f7-list-item>
-                            <f7-list-item link="/dynamic-route/blog/45/post/125/?foo=bar#about" title="Dynamic Route"></f7-list-item>
-                        </f7-list>
-                        <f7-block-title>Side Panels</f7-block-title>
-                        <f7-block>
-                            <f7-grid>
-                                <f7-col width="50">
-                                    <f7-button open-panel="left">Left Panel</f7-button>
-                                </f7-col>
-                                <f7-col width="50">
-                                    <f7-button open-panel="right">Right Panel</f7-button>
-                                </f7-col>
-                            </f7-grid>
-                        </f7-block>
-                        <f7-block-title>Modals</f7-block-title>
-                        <f7-block>
-                            <f7-grid>
-                                <f7-col width="50">
-                                    <f7-button open-popup="#popup">Popup</f7-button>
-                                </f7-col>
-                                <f7-col width="50">
-                                    <f7-button open-login-screen="#login-screen">Login Screen</f7-button>
-                                </f7-col>
-                            </f7-grid>
-                        </f7-block>
-                    </f7-page>
-                </f7-pages>
-            </f7-view>
-        </f7-views>
 
         <!-- Login Screen -->
         <f7-login-screen id="login-screen" :opened="true">
@@ -99,6 +54,65 @@
                 </f7-pages>
             </f7-view>
         </f7-login-screen>
+
+        <!-- Main Views -->
+        <f7-views>
+            <f7-view id="main-view" navbar-through :dynamic-navbar="true" main>
+                <!-- Navbar -->
+                <f7-navbar>
+                    <f7-nav-center sliding>图书馆参考咨询服务</f7-nav-center>
+                    <f7-nav-right>
+                        <f7-link icon="icon-bars" open-panel="right"></f7-link>
+                    </f7-nav-right>
+                </f7-navbar>
+
+                <!-- Pages -->
+                <f7-pages>
+                    <f7-page>
+                        <!-- Search bar -->
+                        <f7-searchbar
+                                cancel-link="Cancel"
+                                search-list="#search-list"
+                                placeholder="Search in items"
+                                :clear-button="true"
+                        ></f7-searchbar>
+
+                        <!-- Will be visible if there is no any results found, defined by "searchbar-not-found" class -->
+                        <f7-list class="searchbar-not-found">
+                            <f7-list-item title="Nothing found"></f7-list-item>
+                        </f7-list>
+
+                        <!-- Search-through list -->
+                        <f7-list media-list class="searchbar-found" id="search-list">
+                            <f7-list-item title="《人民的名义》" subtitle="周梅森" text="北京十月文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《大秦帝国》" subtitle="孙皓晖" text="河南文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《人民的名义》" subtitle="周梅森" text="北京十月文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《大秦帝国》" subtitle="孙皓晖" text="河南文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《人民的名义》" subtitle="周梅森" text="北京十月文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《大秦帝国》" subtitle="孙皓晖" text="河南文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《人民的名义》" subtitle="周梅森" text="北京十月文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《大秦帝国》" subtitle="孙皓晖" text="河南文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《人民的名义》" subtitle="周梅森" text="北京十月文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《大秦帝国》" subtitle="孙皓晖" text="河南文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《人民的名义》" subtitle="周梅森" text="北京十月文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《大秦帝国》" subtitle="孙皓晖" text="河南文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《人民的名义》" subtitle="周梅森" text="北京十月文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《大秦帝国》" subtitle="孙皓晖" text="河南文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《人民的名义》" subtitle="周梅森" text="北京十月文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《大秦帝国》" subtitle="孙皓晖" text="河南文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《人民的名义》" subtitle="周梅森" text="北京十月文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《大秦帝国》" subtitle="孙皓晖" text="河南文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《人民的名义》" subtitle="周梅森" text="北京十月文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《大秦帝国》" subtitle="孙皓晖" text="河南文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《人民的名义》" subtitle="周梅森" text="北京十月文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《大秦帝国》" subtitle="孙皓晖" text="河南文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《人民的名义》" subtitle="周梅森" text="北京十月文艺出版社" link="/about/"></f7-list-item>
+                            <f7-list-item title="《大秦帝国》" subtitle="孙皓晖" text="河南文艺出版社" link="/about/"></f7-list-item>
+                        </f7-list>
+                    </f7-page>
+                </f7-pages>
+            </f7-view>
+        </f7-views>
 
     </div>
 </template>
