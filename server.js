75a7b1117331bbdae4979fa1a7ebc76f53b656bc
neoli <hbbpbb@gmail.com>
2017-06-03 20:23:08 +0800

    deploy.

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var WebpackDevServer = require('webpack-dev-server')
 var config = require('./webpack.config')(env)
 
 new WebpackDevServer(webpack(config), config.devServer)
-  .listen(env.port, 'drsweb.herokuapp.com', (err) => {
+  .listen(env.port, '0.0.0.0', (err) => {
     if (err) {
       console.log(err)
     }
